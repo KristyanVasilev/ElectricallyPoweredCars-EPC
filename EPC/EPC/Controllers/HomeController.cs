bd200b45729936a1392d33d85ba0506c5f9d1b8e
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-07-05 17:52:23 +0300

    Add project files

---
+++ EPC/EPC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using EPC.Core.Constants;
+using EPC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EPC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData[MessageConstant.SuccessMessage] = "Браво!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
