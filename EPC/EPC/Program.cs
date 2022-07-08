0df08cb9bb0ebe6739d52b6874366324d20c7a07
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-07-08 18:21:07 +0300

    Cleaned the project

--- EPC/EPC/Program.cs
+++ EPC/EPC/Program.cs
@@ -24,21 +24,19 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseHttpsRedirection()
+.UseStaticFiles()
+.UseRouting()
+.UseAuthentication()
+.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
