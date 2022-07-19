7415a4d151a5db2bb7c286b647f17f176f7a23a6
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-07-19 15:27:31 +0300

    Added CarsController and AddCarFromModel

---
+++ EPC/EPC.Core/Models/Cars/AddCarFormModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EPC.Infrastructure.Data.DataConstants;
+
+namespace EPC.Core.Models.Cars
+{
+    public class AddCarFormModel
+    {
+        [Required]
+        [StringLength(CarBrandMaxLenght, MinimumLength = CarBrandMinLenght)]
+        public string Brand { get; init; }
+
+        [Required]
+        [MinLength(CarModelMinLenght)]
+        [StringLength(CarModelMaxLenght, MinimumLength = CarModelMinLenght)]
+        public string Model { get; init; }
+
+        [Required]
+        [MinLength(CarDescriptionMinLenght)]
+        [StringLength(CarDescriptionMaxLenght, MinimumLength = CarDescriptionMinLenght)]
+        public string Description { get; init; }
+
+        [Range(CarYearMinValue, CarYearMaxValue)]
+        public int Year { get; init; }
+
+        [Display(Name = "Image Url")]
+        [Required]
+        [Url]
+        public string ImageUrl { get; init; }
+
+        [Display(Name = "Category number")]
+        public int CategoryId { get; init; }
+
+    }
+}
---
+++ EPC/EPC/Controllers/CarsController.cs
@@ -0,0 +1,27 @@
+﻿using EPC.Core.Models.Cars;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace EPC.Controllers
+{
+    public class CarsController : BaseController
+    {
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCarFormModel car)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
