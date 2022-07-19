da0296f2a9aab68ae7ddd50f913a606a56eeb35c
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-07-19 15:27:43 +0300

    Update DataConstants

--- EPC/EPC.Infrastructure/Data/DataConstants.cs
+++ EPC/EPC.Infrastructure/Data/DataConstants.cs
@@ -10,8 +10,18 @@ namespace EPC.Infrastructure.Data
     {
         public const int CarBrandMaxLenght = 20;
 
+        public const int CarBrandMinLenght = 2;
+
         public const int CarModelMaxLenght = 30;
 
+        public const int CarModelMinLenght = 2;
+
         public const int CarDescriptionMaxLenght = 350;
+
+        public const int CarDescriptionMinLenght = 15;
+
+        public const int CarYearMinValue = 2012;
+
+        public const int CarYearMaxValue = 2050;
     }
 }
